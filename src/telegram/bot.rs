a26bcca5eb243eeb2ca335cffc9a1675ed42ba3b
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-09-23 14:52:16 +0300

    feat: display digest on subscription

--- src/telegram/bot.rs
+++ src/telegram/bot.rs
@@ -1,3 +1,4 @@
+use crate::blackout::Blackout;
 use crate::ztoe::check_blackouts;
 use chrono::{Local, NaiveDate, TimeDelta};
 use std::ops::Add;
@@ -14,6 +15,8 @@ enum Command {
     Today,
     #[command(description = "Перевірити відключення на завтра")]
     Tomorrow,
+    #[command(description = "Підписатися на повідомлення про відключення")]
+    Subscribe,
 }
 
 pub async fn init_bot() {
@@ -43,6 +46,11 @@ async fn handle_command(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<
             bot.send_message(msg.chat.id, message)
                 .parse_mode(ParseMode::MarkdownV2)
         }
+        Command::Subscribe => {
+            let message = forecast_digest().await;
+            bot.send_message(msg.chat.id, message)
+                .parse_mode(ParseMode::MarkdownV2)
+        }
     }
     .await?;
     Ok(())
@@ -50,6 +58,24 @@ async fn handle_command(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<
 
 async fn forecast_for(date: NaiveDate) -> String {
     let blackouts = check_blackouts(date).await;
+    forecast_message(date, &blackouts)
+}
+
+async fn forecast_digest() -> String {
+    let dates = [
+        Local::now().date_naive(),
+        Local::now().date_naive().add(TimeDelta::days(1)),
+    ];
+    let mut messages = vec![];
+    for date in dates {
+        let blackouts = check_blackouts(date).await;
+        let message = forecast_message(date, &blackouts);
+        messages.push(message);
+    }
+    messages.join("\n")
+}
+
+fn forecast_message(date: NaiveDate, blackouts: &[Blackout]) -> String {
     if blackouts.is_empty() {
         format!("✅ `{}` відключення не прогнозуються\\.", date)
     } else {
